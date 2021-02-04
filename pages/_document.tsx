73ca2ed3de38bc12381d93ece96dea5afda6af75
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-02-04 15:53:22 +0300

    NextJS Document Override and remove globals.css

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
