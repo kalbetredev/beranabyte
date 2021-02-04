dae58e880cd78818f378a481837980b45b1e40bb
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-02-04 15:40:18 +0300

    project init

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Berana Byte</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Berana Byte</h1>
+      </main>
+    </div>
+  );
+}
