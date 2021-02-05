9ad9a361aafbe4311a15eb6f17e87898800f4375
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-02-05 11:09:14 +0300

    redux integration to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,19 @@
 import { useEffect } from "react";
 import AppTheme from "../shared/theme/app-theme";
-import { ThemeType } from "../shared/theme/theme-type";
 import { ThemeProvider } from "@material-ui/core/styles";
 import "../shared/styles/globals.css";
+import { RootState } from "../redux/rootReducer";
+import { useSelector } from "react-redux";
+import MDXComponents from "../components/MDX/MDXComponents";
+import { MDXProvider } from "@mdx-js/react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { wrapper } from "../redux/store";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
+  const themeType = useSelector((state: RootState) => state.theme.themeType);
+  const theme = AppTheme(themeType);
+
   useEffect(() => {
     // Material-UI Fix Remove the server-side injected CSS
     const jssStyles = document.querySelector("#jss-server-side");
@@ -13,13 +22,22 @@ function MyApp({ Component, pageProps }) {
     }
   }, []);
 
-  const theme = AppTheme(ThemeType.DARK);
-
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <MDXProvider components={MDXComponents}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </MDXProvider>
+      </ThemeProvider>
+    </>
   );
 }
 
-export default MyApp;
+export default wrapper.withRedux(MyApp);
