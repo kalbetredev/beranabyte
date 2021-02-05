eb9c73171e67ce19b017bc4def0391a4a53b500a
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-02-05 11:08:13 +0300

    removed viewport meta from _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,12 +4,7 @@ export class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width"
-          />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
