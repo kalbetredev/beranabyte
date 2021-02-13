f4bcfe65267e6360c8dc7d268b615b314997d169
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-02-13 11:18:15 +0300

    user management removed from redux

--- redux/rootReducer.ts
+++ redux/rootReducer.ts
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
 import { ThemeReducer } from "./theme/reducers";
-import { UserAccountReducer } from "./user/reducers";
 
 const rootReducer = combineReducers({
   theme: ThemeReducer,
-  userAccount: UserAccountReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
