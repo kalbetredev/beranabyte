5786161c1085ce3db4754a41a5a4de409cae2de7
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-06-15 16:54:37 +0300

    Using fixed height to fix appbar

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -1,14 +1,10 @@
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import { Toolbar } from "@material-ui/core";
 import AppBar from "./AppBar";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     toolbar: {
-      minHeight: theme.mixins.toolbar.minHeight,
-      [`${theme.breakpoints.up("xs")} and (orientation: portrait)`]: {
-        minHeight: 80,
-      },
+      height: 65,
     },
   })
 );
