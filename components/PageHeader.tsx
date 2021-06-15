df3bc11b1aaea8841123787819b11c8604d413fe
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-06-15 16:35:35 +0300

    using mixins to fix appbar

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -2,20 +2,24 @@ import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import { Toolbar } from "@material-ui/core";
 import AppBar from "./AppBar";
 
-// const useStyles = makeStyles((theme: Theme) =>
-//   createStyles({
-//     toolbar: theme.mixins.toolbar,
-//   })
-// );
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    toolbar: {
+      minHeight: theme.mixins.toolbar.minHeight,
+      [`${theme.breakpoints.up("xs")} and (orientation: portrait)`]: {
+        minHeight: 80,
+      },
+    },
+  })
+);
 
 const PageHeader: React.FC = () => {
-  // const classes = useStyles();
+  const classes = useStyles();
 
   return (
     <>
       <AppBar />
-      <Toolbar />
-      {/* <div className={classes.toolbar} /> */}
+      <div className={classes.toolbar} />
     </>
   );
 };
