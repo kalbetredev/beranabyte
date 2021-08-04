fb63e268586784785c08234b8d206757c92637fe
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-08-04 23:31:44 +0300

    new icons based on heroicons

---
+++ icons/index.ts
@@ -0,0 +1,7 @@
+import DarkThemeIcon from "./DarkThemeIcon";
+import LightThemeIcon from "./LightThemeIcon";
+import MenuIcon from "./MenuIcon";
+import MoreIcon from "./MoreIcon";
+import UserIcon from "./UserIcon";
+
+export { DarkThemeIcon, LightThemeIcon, MenuIcon, MoreIcon, UserIcon };
