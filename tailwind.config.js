bc64989f1e4888c599bc7d59d163ad26b10e52ab
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-08-04 23:32:19 +0300

    theme colors and purge for icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./icons/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: "class",
   theme: {
     extend: {
       backgroundColor: {
+        brand: "teal",
+        light: "white",
+        dark: "#212121",
+      },
+      textColor: {
         brand: "teal",
       },
     },
