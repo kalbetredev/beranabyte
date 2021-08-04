110d73d2f0d8f7b0c0195f609998ba08c2b1eabf
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-08-04 22:16:05 +0300

    add jit support and brand color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 module.exports = {
+  mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        brand: "teal",
+      },
+    },
   },
   variants: {
     extend: {},
