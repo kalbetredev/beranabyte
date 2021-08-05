480e2549dc20dd2c9718326fc66e17a4ac3d2d1c
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-08-05 15:34:25 +0300

    social media icons

--- icons/index.ts
+++ icons/index.ts
@@ -3,5 +3,21 @@ import LightThemeIcon from "./LightThemeIcon";
 import MenuIcon from "./MenuIcon";
 import MoreIcon from "./MoreIcon";
 import UserIcon from "./UserIcon";
+import GitHubIcon from "./GitHub";
+import LinkedInIcon from "./LinkedInIcon";
+import TelegramIcon from "./TelegramIcon";
+import MailIcon from "./MailIcon";
+import FacebookIcon from "./FacebookIcon";
 
-export { DarkThemeIcon, LightThemeIcon, MenuIcon, MoreIcon, UserIcon };
+export {
+  DarkThemeIcon,
+  LightThemeIcon,
+  MenuIcon,
+  MoreIcon,
+  UserIcon,
+  GitHubIcon,
+  LinkedInIcon,
+  TelegramIcon,
+  MailIcon,
+  FacebookIcon,
+};
