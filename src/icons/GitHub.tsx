99d69bee5fddca77258bee1f1eed7fae250a7f97
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-08-08 22:13:19 +0300

    add react FC type for icons

--- src/icons/GitHub.tsx
+++ src/icons/GitHub.tsx
@@ -1,4 +1,4 @@
-const GitHubIcon = () => {
+const GitHubIcon: React.FC = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
--- src/icons/TelegramIcon.tsx
+++ src/icons/TelegramIcon.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const TelegramIcon = () => {
+const TelegramIcon: React.FC = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
