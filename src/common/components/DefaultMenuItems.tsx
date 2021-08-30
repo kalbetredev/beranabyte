d455bab4bdf926032227c188f70bb1e594a5b1a9
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-08-30 17:14:14 +0300

    update to default menu on small screens

--- src/common/components/DefaultMenuItems.tsx
+++ src/common/components/DefaultMenuItems.tsx
@@ -1,33 +1,41 @@
 import React from "react";
 import MenuItem from "./MenuItem";
-import { PencilAltIcon, LoginIcon } from "@heroicons/react/solid";
+import { PencilAltIcon, LoginIcon, CodeIcon } from "@heroicons/react/solid";
+import { NewspaperIcon } from "@heroicons/react/outline";
 import { InformationCircleIcon, ChatIcon } from "@heroicons/react/outline";
 
 const DefaultMenuItems = () => {
   return (
     <>
+      <div className="px-1 py-1 sm:hidden">
+        <MenuItem
+          icon={<NewspaperIcon className="w-5 h-5" aria-hidden="true" />}
+          label="Blogs"
+        />
+        <MenuItem
+          icon={<CodeIcon className="w-5 h-5" aria-hidden="true" />}
+          label="Projects"
+        />
+      </div>
       <div className="px-1 py-1 ">
         <MenuItem
-          icon={<PencilAltIcon className="w-5 h-5 mr-2" aria-hidden="true" />}
+          icon={<PencilAltIcon className="w-5 h-5" aria-hidden="true" />}
           label="Register"
         />
         <MenuItem
-          icon={<LoginIcon className="w-5 h-5 mr-2" aria-hidden="true" />}
+          icon={<LoginIcon className="w-5 h-5" aria-hidden="true" />}
           label="Sign In"
         />
       </div>
       <div className="px-1 py-1">
         <MenuItem
           icon={
-            <InformationCircleIcon
-              className="w-5 h-5 mr-2"
-              aria-hidden="true"
-            />
+            <InformationCircleIcon className="w-5 h-5" aria-hidden="true" />
           }
           label="About"
         />
         <MenuItem
-          icon={<ChatIcon className="w-5 h-5 mr-2" aria-hidden="true" />}
+          icon={<ChatIcon className="w-5 h-5" aria-hidden="true" />}
           label="Contact Kalkidan B."
         />
       </div>
