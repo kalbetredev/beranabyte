ed2c631d0d377765a53c9eab67b30f9170221e76
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-08-30 15:46:45 +0300

    blog collection component

---
+++ src/common/components/BlogCollection.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import BlogSummary from "../types/BlogSummary";
+import BlogItem from "./BlogItem";
+
+interface BlogCollectionProps {
+  title: string;
+  blogs: BlogSummary[];
+}
+
+const BlogCollection: React.FC<BlogCollectionProps> = (
+  props: BlogCollectionProps
+) => {
+  return (
+    <div>
+      <h2 className="uppercase py-3 mb-4 text-xl border-b border-gray-400 border-opacity-40">
+        {props.title}
+      </h2>
+      {props.blogs.map((blog) => (
+        <BlogItem blogSummary={blog} />
+      ))}
+    </div>
+  );
+};
+
+export default BlogCollection;
