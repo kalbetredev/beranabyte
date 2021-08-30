51cc32f965c08f202e61e0ba78ffaf3416ffd2ab
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-08-30 14:32:02 +0300

    search input control style update for focus

--- src/common/components/SearchInput.tsx
+++ src/common/components/SearchInput.tsx
@@ -10,6 +10,7 @@ interface SearchInputProps {
 const SearchInput: React.FC<SearchInputProps> = (props: SearchInputProps) => {
   const placeholder = props.placeholder ?? "Search";
   const [searchInput, setSearchInput] = useState("");
+  const [focus, setFocus] = useState(false);
 
   const handelInputChange = (event: React.FormEvent<HTMLInputElement>) => {
     let value = event.currentTarget.value;
@@ -24,9 +25,16 @@ const SearchInput: React.FC<SearchInputProps> = (props: SearchInputProps) => {
   };
 
   return (
-    <div className="relative">
+    <div className="relative w-full sm:w-72">
       <div className="absolute inset-y-0 left-0 flex items-center pl-2">
-        <SearchIcon className="w-5 h-5 text-gray-300" />
+        <SearchIcon
+          className={
+            "w-5 h-5 " +
+            (focus
+              ? "text-brand text-opacity-30 dark:text-opacity-100"
+              : "text-gray-300")
+          }
+        />
       </div>
       <input
         type="text"
@@ -35,7 +43,9 @@ const SearchInput: React.FC<SearchInputProps> = (props: SearchInputProps) => {
         value={searchInput}
         onInput={handelInputChange}
         onKeyUp={onSearch}
-        className="outline-none pl-8 bg-transparent text-sm w-full sm:w-72 border rounded-md flex justify-center items-center p-[6px] border-gray-400 border-opacity-25 focus:ring-1 ring-brand ring-opacity-50"
+        onFocus={() => setFocus(true)}
+        onBlur={() => setFocus(false)}
+        className="outline-none pl-8 bg-transparent text-sm w-full border rounded-md flex justify-center items-center p-[6px] border-gray-400 border-opacity-25 focus:border-brand focus:border-opacity-25 focus:ring-brand focus:ring-opacity-50"
       />
     </div>
   );
