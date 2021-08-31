24a83b6173cfaed59a893e5aa9e35dc68a165667
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-08-31 19:13:32 +0300

    button controls with variants

---
+++ src/common/components/SecondaryButton.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import React from "react";
+
+interface SecondaryButtonProps {
+  slug?: string;
+  onClick?: () => void;
+  label: string;
+}
+
+const SecondaryButton: React.FC<SecondaryButtonProps> = (
+  props: SecondaryButtonProps
+) => {
+  const className =
+    "w-full flex my-2 justify-center py-2 px-4 border border-gray-500 rounded-md text-sm font-medium text-white hover:bg-gray-500 outline-none focus:ring-2 focus:ring-offset-2 dark:ring-offset-gray-700 focus:ring-brand-light cursor-default";
+
+  return (
+    <>
+      {props.slug ? (
+        <div className={className}>
+          <Link href={props.slug}>
+            <a className="w-full text-sm text-center cursor-default">
+              {props.label}
+            </a>
+          </Link>
+        </div>
+      ) : (
+        <button onClick={props.onClick} className={className}>
+          {props.label}
+        </button>
+      )}
+    </>
+  );
+};
+
+export default SecondaryButton;
