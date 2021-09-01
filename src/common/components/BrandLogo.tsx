85e43b7edd17bf0552e1f34ff215ba790df0a505
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-09-01 15:58:19 +0300

    brand logo icons

---
+++ src/common/components/BrandLogo.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { ExclamationIcon } from "@heroicons/react/solid";
+import NodeJsIcon from "../../icons/NodeJsIcon";
+import PythonIcon from "../../icons/PythonIcon";
+
+const iconLabels = ["python", "nodejs"];
+const icons = [PythonIcon, NodeJsIcon];
+
+interface BrandLogoProps {
+  label: string;
+}
+
+const BrandLogo: React.FC<BrandLogoProps> = (props: BrandLogoProps) => {
+  const index = iconLabels.indexOf(props.label.toLowerCase());
+  return index >= 0 ? (
+    icons[index]()
+  ) : (
+    <span className="text-yellow-400 dark:text-yellow-600 w-8 h-8">
+      <ExclamationIcon />
+    </span>
+  );
+};
+
+export default BrandLogo;
