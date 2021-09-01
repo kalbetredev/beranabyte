c98788706c9553f360e67c9dbdf492d482b5fadd
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-09-01 08:22:34 +0300

    use brand color for active link

--- src/common/components/AppBarLink.tsx
+++ src/common/components/AppBarLink.tsx
@@ -12,7 +12,7 @@ const AppBarLink: React.FC<AppBarLinkProps> = (props: AppBarLinkProps) => {
       <div className="relative flex justify-center items-center w-20 h-6 hover:bg-gray-400 hover:bg-opacity-10 hover:text-brand cursor-pointer">
         <a className="text-xs text-center uppercase">{props.pageLink.label}</a>
         {props.active ? (
-          <div className="absolute -bottom-3 w-20 h-1 bg-gray-500 rounded-t-full"></div>
+          <div className="absolute -bottom-3 w-20 h-1 bg-brand-light dark:bg-brand-dark rounded-t-full"></div>
         ) : null}
       </div>
     </Link>
