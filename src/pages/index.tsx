624c11092138de121c2f4b90602fb74e3e184d96
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-09-03 15:52:28 +0300

    index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,74 +1,109 @@
 import React from "react";
+import Link from "next/link";
+import BlogLinks from "../common/components/BlogLinks";
+import FormInputWithButton from "../common/components/FormInputWithButton";
+import ProjectCollection from "../common/components/ProjectCollection";
+import Subscribe from "../common/components/Subscribe";
+import { REGISTER_PAGE_SLUG } from "../common/constants/page-slugs";
 import Page from "../common/layouts/Page";
-import PageMeta from "../common/types/PageMeta";
+import { BlogCategory } from "../common/enums/BlogCategory";
+import { ProjectType } from "../common/enums/ProjectType";
 
-//TODO: Blogs on Home Page
-// import { FEATURED } from "../../constants/strings";
-// import BlogsContainer from "../../components/MUI_BlogsContainer";
-
-const Index = () => {
-  const meta: PageMeta = {
-    title: "BeranaByte",
-    description:
-      "Blog / portfolio website where you can find blogs and projects on most recent technologies on software development and other tech things.",
-    type: "website",
-    image: "/static/images/banner.png",
-  };
-
+const Index: React.FC = () => {
   return (
-    <Page meta={meta}>
-      {/* <BlogsContainer category={FEATURED} /> */}
-      <div className="mt-4 mb-8">
-        <h1>BeranaByte</h1>
-
-        <p>
-          What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing
-          and typesetting industry. Lorem Ipsum has been the industry's standard
-          dummy text ever since the 1500s, when an unknown printer took a galley
-          of type and scrambled it to make a type specimen book. It has survived
-          not only five centuries, but also the leap into electronic
-          typesetting, remaining essentially unchanged. It was popularised in
-          the 1960s with the release of Letraset sheets containing Lorem Ipsum
-          passages, and more recently with desktop publishing software like
-          Aldus PageMaker including versions of Lorem Ipsum. Why do we use it?
-          It is a long established fact that a reader will be distracted by the
-          readable content of a page when looking at its layout. The point of
-          using Lorem Ipsum is that it has a more-or-less normal distribution of
-          letters, as opposed to using 'Content here, content here', making it
-          look like readable English. Many desktop publishing packages and web
-          page editors now use Lorem Ipsum as their default model text, and a
-          search for 'lorem ipsum' will uncover many web sites still in their
-          infancy. Various versions have evolved over the years, sometimes by
-          accident, sometimes on purpose (injected humour and the like). Where
-          does it come from? Contrary to popular belief, Lorem Ipsum is not
-          simply random text. It has roots in a piece of classical Latin
-          literature from 45 BC, making it over 2000 years old. Richard
-          McClintock, a Latin professor at Hampden-Sydney College in Virginia,
-          looked up one of the more obscure Latin words, consectetur, from a
-          Lorem Ipsum passage, and going through the cites of the word in
-          classical literature, discovered the undoubtable source. Lorem Ipsum
-          comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et
-          Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC.
-          This book is a treatise on the theory of ethics, very popular during
-          the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
-          amet..", comes from a line in section 1.10.32. The standard chunk of
-          Lorem Ipsum used since the 1500s is reproduced below for those
-          interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et
-          Malorum" by Cicero are also reproduced in their exact original form,
-          accompanied by English versions from the 1914 translation by H.
-          Rackham. Where can I get some? There are many variations of passages
-          of Lorem Ipsum available, but the majority have suffered alteration in
-          some form, by injected humour, or randomised words which don't look
-          even slightly believable. If you are going to use a passage of Lorem
-          Ipsum, you need to be sure there isn't anything embarrassing hidden in
-          the middle of text. All the Lorem Ipsum generators on the Internet
-          tend to repeat predefined chunks as necessary, making this the first
-          true generator on the Internet. It uses a dictionary of over 200 Latin
-          words, combined with a handful of model sentence structures, to
-          generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum
-          is therefore always free from repetition, injected humour, or
-          non-characteristic words etc.
-        </p>
+    <Page>
+      <div className="mt-4 mb-8 w-full">
+        <div className="w-full mt-16 mb-8">
+          <h1 className="text-7xl font-medium mt-20">Welcome</h1>
+          <div className="max-w-xl">
+            <h2 className="text-sm mt-5 text-gray-400">
+              I'm Kalkidan B. and you have landed on{" "}
+              <span className="text-brand font-bold">BeranaByte.</span> You will
+              find here my personal blogs and projects. I'm a developer and I
+              built this site to share what I have learned through building
+              various projects and researching popular tech stacks.
+            </h2>
+            <p className="text-sm mt-4 text-gray-400 ">
+              Feel free to leave me a message, a comment or a request!.
+            </p>
+            <p className="text-sm mt-4 text-gray-400 ">
+              You can also register to comment on blogs and receive email
+              notifications when I publish new articles
+            </p>
+          </div>
+        </div>
+        <div className="border-t w-full dark:border-gray-700 mt-10 pt-10">
+          <div className="w-full flex flex-wrap">
+            <div className="w-full md:w-1/2 md:pr-2">
+              <BlogLinks
+                title="Most Viewed Blogs"
+                category={BlogCategory.MOST_VIEWED}
+                count={3}
+              />
+            </div>
+            <div className="w-full md:w-1/2 md:pl-2">
+              <BlogLinks
+                title="Latest Blogs"
+                category={BlogCategory.LATEST}
+                count={3}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="border-t w-full dark:border-gray-700 mt-6">
+          <div className="flex flex-col md:flex-row md:mt-8">
+            <div className="flex-1">
+              <ProjectCollection
+                title="Recent Projects"
+                featured
+                count={3}
+                hideTitleDivider
+              />
+            </div>
+            <div className="w-full md:ml-10 border-t border-gray-300 dark:border-gray-700 pt-8 mt-4 md:pt-0 md:mt-14 md:w-[280px] md:border-none">
+              <BlogLinks
+                title="Related Blogs"
+                category={BlogCategory.PROJECT_RELATED}
+                count={3}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="border-t w-full dark:border-gray-700 mt-8 pt-10">
+          <div className="flex flex-col md:flex-row mt-1">
+            <div className="flex-1">
+              <Subscribe />
+            </div>
+            <div className="w-full md:ml-10 mt-4 md:mt-0 md:w-[280px] hidden md:block">
+              <h2 className="text-xl mb-1 mt-3 md:mt-0">Your Can Also ...</h2>
+              <p className="text-sm text-gray-400 dark:text-gray-300">
+                <Link href={REGISTER_PAGE_SLUG}>
+                  <a className="font-bold text-brand hover:text-brand-light">
+                    Sign Up
+                  </a>
+                </Link>{" "}
+                to be a member which will allow you to comment on blogs besides
+                being subscribed to the mailing list for updates!
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row mt-10">
+            <div className="flex-1">
+              <div className="shadow max-w-2xl p-5 rounded-lg border dark:border-gray-700 ">
+                <h2 className="text-xl">For Your Quick thoughts ...</h2>
+                <FormInputWithButton
+                  type="text"
+                  name="message"
+                  id="anonymous_message"
+                  placeholder="Leave a message ..."
+                  label="Send"
+                  className="mt-3"
+                />
+              </div>
+            </div>
+            <div className="w-full md:ml-10 mt-4 md:mt-0 md:w-[280px] hidden md:block"></div>
+          </div>
+        </div>
       </div>
     </Page>
   );
