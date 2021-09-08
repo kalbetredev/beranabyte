3ae2328bfd83d9cb777090c1c05e97fe85620f3c
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-09-08 11:41:42 +0300

    submission form state handling & validation

--- src/common/components/SubscriptionForm.tsx
+++ src/common/components/SubscriptionForm.tsx
@@ -1,16 +1,29 @@
 import React, { useState } from "react";
+import { isEmailValid } from "../utils/input-validation";
+import FormErrorMessage from "./FormErrorMessage";
 import FormInputWithButton from "./FormInputWithButton";
 
 const SubscriptionForm = () => {
   const [email, setEmail] = useState("");
+  const [error, setError] = useState(false);
+  const [validateOnChange, setValidateOnChange] = useState(false);
 
   const handelInputChange = (event: React.FormEvent<HTMLInputElement>) => {
     setEmail(event.currentTarget.value);
+    if (validateOnChange) setError(!isEmailValid(event.currentTarget.value));
   };
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    console.log(email);
+    if (!isEmailValid(email)) {
+      setError(true);
+      setValidateOnChange(true);
+    } else {
+      setError(false);
+
+      //TODO : Submit Email To Server
+      console.log(email);
+    }
   };
 
   return (
@@ -21,14 +34,20 @@ const SubscriptionForm = () => {
       </p>
       <form action="" onSubmit={handleSubmit}>
         <FormInputWithButton
-          inputType="email"
+          inputType="text"
           inputValue={email}
           inputOnChange={handelInputChange}
           inputPlaceholder="Please Enter Your Email"
           btnType="submit"
           btnLabel="subscribe"
           className="mt-3"
+          errorState={error}
         />
+        {error ? (
+          <div className="mt-3">
+            <FormErrorMessage message="Invalid Email Address" />
+          </div>
+        ) : null}
       </form>
     </div>
   );
