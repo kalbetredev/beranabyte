3e48dd551c3ccca3d9f66d78a9c14c8d35070140
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-09-09 09:42:48 +0300

    blog categories fetched using useBlogs hook

--- src/common/hooks/useBlogs.ts
+++ src/common/hooks/useBlogs.ts
@@ -1,17 +1,42 @@
 import useSWR from "swr";
 import { BLOGS_API_ENDPOINT } from "../../api/endpoints";
-import { BlogCategory } from "../enums/BlogCategory";
+import BlogMeta from "../types/BlogMeta";
 
-const useBlogs = (
-  count: number,
-  category: BlogCategory,
-  topic: string = ""
-) => {
-  const apiEndPoint = `${BLOGS_API_ENDPOINT}/?category=${category}&count=${count}&topic=${topic}`;
-  const { data, error } = useSWR(apiEndPoint);
+const useBlogs = () => {
+  const { data, error } = useSWR(BLOGS_API_ENDPOINT);
+
+  const featuredBlogs: BlogMeta[] = data
+    ? data.blogs.filter((blog) => blog.isFeatured == true)
+    : [];
+
+  const latestBlogs: BlogMeta[] = data
+    ? data.blogs
+        .slice()
+        .sort(
+          (a, b) =>
+            new Date(a.publishedOn).getTime() -
+            new Date(b.publishedOn).getTime()
+        )
+        .slice(0, 5)
+    : [];
+
+  const mostViewedBlogs: BlogMeta[] = data
+    ? data.blogs
+        .slice()
+        .sort((a, b) => a.viewCount - b.viewCount)
+        .slice(0, 5)
+    : [];
+
+  const projectRelatedBlogs: BlogMeta[] = data
+    ? data.blogs.filter((blog) => blog.linkedProjects.length > 0)
+    : [];
 
   return {
     blogs: data ? data.blogs : data,
+    featuredBlogs: featuredBlogs,
+    latestBlogs: latestBlogs,
+    mostViewedBlogs: mostViewedBlogs,
+    projectRelatedBlogs: projectRelatedBlogs,
     isLoading: !error && !data,
     isError: error,
   };
