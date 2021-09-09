f9b7e379336d41ac25315a1f77f9d08fe8eaa4cd
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-09-09 10:36:00 +0300

    various project collections display & searching

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import BlogLinks from "../common/components/BlogLinks";
 import ProjectCollection from "../common/components/ProjectCollection";
 import SearchInput from "../common/components/SearchInput";
-import { ProjectType } from "../common/enums/ProjectType";
 import useBlogs from "../common/hooks/useBlogs";
+import useProjects from "../common/hooks/useProjects";
 import Page from "../common/layouts/Page";
+import Project from "../common/types/Project";
 
 const ProjectsPage = () => {
   const blogData = useBlogs();
+  const projects = useProjects();
+
+  const [isSearching, setIsSearching] = useState(false);
+  const [searchResult, setSearchResult] = useState<Project[]>([]);
 
   const onSearch = (search: string) => {
-    console.log(search);
+    if (search == "") {
+      setIsSearching(false);
+    } else {
+      setIsSearching(true);
+      const pattern = new RegExp(search, "i");
+      setSearchResult(
+        projects.projects
+          ? projects.projects.filter(
+              (project) =>
+                project.title.search(pattern) >= 0 ||
+                project.summary.search(pattern) >= 0
+            )
+          : []
+      );
+    }
   };
 
   return (
@@ -25,11 +44,39 @@ const ProjectsPage = () => {
         </div>
         <div className="flex flex-col md:flex-row md:mt-10">
           <div className="flex-1">
-            <ProjectCollection
-              title="Open Source Projects"
-              type={ProjectType.OPEN_SOURCE}
-              count={3}
-            />
+            {isSearching ? (
+              <ProjectCollection
+                title="Matching Projects"
+                projects={searchResult}
+                isLoading={projects.isLoading}
+                isError={projects.isError}
+              />
+            ) : (
+              <>
+                <ProjectCollection
+                  title="Open Source Projects"
+                  projects={projects.openSource}
+                  isLoading={projects.isLoading}
+                  isError={projects.isError}
+                />
+                {projects.contributions.length > 0 ? (
+                  <ProjectCollection
+                    title="Contributions"
+                    projects={projects.contributions}
+                    isLoading={projects.isLoading}
+                    isError={projects.isError}
+                  />
+                ) : null}
+                {projects.private.length > 0 ? (
+                  <ProjectCollection
+                    title="Private Projects"
+                    projects={projects.private}
+                    isLoading={projects.isLoading}
+                    isError={projects.isError}
+                  />
+                ) : null}
+              </>
+            )}
           </div>
           <div className="w-full md:ml-10 border-t border-gray-300 dark:border-gray-700 pt-8 mt-4 md:pt-0 md:mt-14 md:w-[280px] md:border-none">
             <BlogLinks
