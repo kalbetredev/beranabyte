e67f80521966df77b567db456ead801e6254c52c
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-09-09 10:34:34 +0300

    data fetching moved out of project collection component

--- src/common/components/ProjectCollection.tsx
+++ src/common/components/ProjectCollection.tsx
@@ -1,7 +1,5 @@
 import { ExclamationIcon } from "@heroicons/react/outline";
 import React from "react";
-import { ProjectType } from "../enums/ProjectType";
-import useProjects from "../hooks/useProjects";
 import Project from "../types/Project";
 import ProjectItem from "./ProjectItem";
 import ProjectItemLoading from "./ProjectItemLoading";
@@ -9,25 +7,19 @@ import ProjectItemLoading from "./ProjectItemLoading";
 interface ProjectCollectionProps {
   title: string;
   hideTitleDivider?: boolean;
-  featured?: boolean;
-  type?: ProjectType;
-  count: number;
+  projects: Project[];
+  isLoading: boolean;
+  isError: boolean;
 }
 
 const ProjectCollection: React.FC<ProjectCollectionProps> = (
   props: ProjectCollectionProps
 ) => {
-  const { projects, isLoading, isError } = useProjects(
-    props.type,
-    props.count,
-    props.featured
-  );
-
   const borderStyle = props.hideTitleDivider
     ? ""
     : "border-b border-gray-300 dark:border-gray-700";
 
-  if (isLoading)
+  if (props.isLoading)
     return (
       <div>
         <h2 className={`uppercase py-3 mb-4 text-xl ` + borderStyle}>
@@ -43,13 +35,13 @@ const ProjectCollection: React.FC<ProjectCollectionProps> = (
       <h2 className={`uppercase py-3 mb-4 text-xl ` + borderStyle}>
         {props.title}
       </h2>
-      {isError ? (
+      {props.isError ? (
         <h3 className="text-xs text-red-900 dark:text-red-300 mb-2 flex">
           <ExclamationIcon className="w-4 h-4 text-red-400 mr-1" />
           Error loading {props.title}
         </h3>
       ) : (
-        projects.map((project: Project) => (
+        props.projects.map((project: Project) => (
           <ProjectItem key={project._id} project={project} />
         ))
       )}
@@ -59,7 +51,6 @@ const ProjectCollection: React.FC<ProjectCollectionProps> = (
 
 ProjectCollection.defaultProps = {
   hideTitleDivider: false,
-  type: ProjectType.ANY,
 };
 
 export default ProjectCollection;
