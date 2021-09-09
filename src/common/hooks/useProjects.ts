63dceac8a21713d8a17819847bf173552fb25fdc
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-09-09 10:34:11 +0300

    fetching all projects using useProjects hook

--- src/common/hooks/useProjects.ts
+++ src/common/hooks/useProjects.ts
@@ -1,19 +1,35 @@
 import useSWR from "swr";
 import { PROJECTS_API_ENDPOINT } from "../../api/endpoints";
 import { ProjectType } from "../enums/ProjectType";
+import Project from "../types/Project";
 
-const useProjects = (
-  type: ProjectType,
-  count: number,
-  isFeatured: boolean | null = null
-) => {
-  const apiEndPoint =
-    `${PROJECTS_API_ENDPOINT}?type=${type}&count=${count}` +
-    (isFeatured ? `&featured=${isFeatured}` : "");
-  const { data, error } = useSWR(apiEndPoint);
+const useProjects = () => {
+  const { data, error } = useSWR(PROJECTS_API_ENDPOINT);
+
+  const featuredProjects: Project[] = data
+    ? data.projects.filter((project) => project.isFeatured == true)
+    : [];
+
+  const openSourceProjects: Project[] = data
+    ? data.projects.filter((project) => project.type == ProjectType.OPEN_SOURCE)
+    : [];
+
+  const privateProjects: Project[] = data
+    ? data.projects.filter((project) => project.type == ProjectType.PRIVATE)
+    : [];
+
+  const contributionProjects: Project[] = data
+    ? data.projects.filter(
+        (project) => project.type == ProjectType.CONTRIBUTION
+      )
+    : [];
 
   return {
     projects: data ? data.projects : data,
+    featured: featuredProjects,
+    openSource: openSourceProjects,
+    private: privateProjects,
+    contributions: contributionProjects,
     isLoading: !error && !data,
     isError: error,
   };
