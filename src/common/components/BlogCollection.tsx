065d894388d339384b12dbdf0303a3b7733e67e2
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-09-09 09:44:38 +0300

    data fetching moved out of blog collection

--- src/common/components/BlogCollection.tsx
+++ src/common/components/BlogCollection.tsx
@@ -1,29 +1,22 @@
 import { ExclamationIcon } from "@heroicons/react/outline";
 import React from "react";
-import { BlogCategory } from "../enums/BlogCategory";
-import useBlogs from "../hooks/useBlogs";
+import BlogMeta from "../types/BlogMeta";
 import BlogItem from "./BlogItem";
 import BlogItemLoading from "./BlogItemLoading";
 
 interface BlogCollectionProps {
   title: string;
-  count: number;
-  category: BlogCategory;
-  topic?: string;
+  blogs: BlogMeta[];
+  isLoading: boolean;
+  isError: boolean;
 }
 
 const BlogCollection: React.FC<BlogCollectionProps> = (
   props: BlogCollectionProps
 ) => {
-  const { blogs, isLoading, isError } = useBlogs(
-    props.count,
-    props.category,
-    props.topic
-  );
-
-  if (isLoading)
+  if (props.isLoading)
     return (
-      <div>
+      <div className="w-full">
         <h2 className="uppercase py-3 mb-4 text-xl border-b border-gray-300 dark:border-gray-700">
           {props.title}
         </h2>
@@ -34,24 +27,20 @@ const BlogCollection: React.FC<BlogCollectionProps> = (
     );
 
   return (
-    <div>
+    <div className="w-full">
       <h2 className="uppercase py-3 mb-4 text-xl border-b border-gray-300 dark:border-gray-700">
         {props.title}
       </h2>
-      {isError ? (
+      {props.isError ? (
         <h3 className="text-xs text-red-900 dark:text-red-300 mb-2 flex">
           <ExclamationIcon className="w-4 h-4 text-red-400 mr-1" />
           Error loading {props.title} blogs
         </h3>
       ) : (
-        blogs.map((blog) => <BlogItem key={blog._id} blog={blog} />)
+        props.blogs.map((blog) => <BlogItem key={blog._id} blog={blog} />)
       )}
     </div>
   );
-};
-
-BlogCollection.defaultProps = {
-  topic: "",
 };
 
 export default BlogCollection;
