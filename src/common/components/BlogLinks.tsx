abc953c7fbd77c6dec4b3c53c888b1f65ca96e79
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-09-09 09:44:12 +0300

    loading and error state for blog links

--- src/common/components/BlogLinks.tsx
+++ src/common/components/BlogLinks.tsx
@@ -1,32 +1,30 @@
+import React from "react";
 import { ExclamationIcon } from "@heroicons/react/outline";
 import Link from "next/link";
-import React from "react";
-import { BlogCategory } from "../enums/BlogCategory";
-import useBlogs from "../hooks/useBlogs";
+import BlogMeta from "../types/BlogMeta";
 import BlogLinksLoading from "./BlogLinksLoading";
 
 interface BlogLinksProps {
   title: string;
-  category: BlogCategory;
-  count: number;
+  blogs: BlogMeta[];
+  isLoading: boolean;
+  isError: boolean;
 }
 
 const BlogLinks: React.FC<BlogLinksProps> = (props: BlogLinksProps) => {
-  const { blogs, isLoading, isError } = useBlogs(props.count, props.category);
-
-  if (isLoading) return <BlogLinksLoading title={props.title} />;
+  if (props.isLoading) return <BlogLinksLoading title={props.title} />;
 
   return (
     <div className="mb-6 border-l border-gray-400 rounded-2xl px-3 py-1">
       <div>
         <h2 className="text-lg mb-2">{props.title}</h2>
-        {isError ? (
+        {props.isError ? (
           <h3 className="text-xs text-red-900 dark:text-red-300 mb-2 flex">
             <ExclamationIcon className="w-4 h-4 text-red-400 mr-1" />
             Error loading {props.title}
           </h3>
         ) : (
-          blogs.map((blog) => (
+          props.blogs.map((blog) => (
             <h3 key={blog._id} className="text-xs text-gray-400 mb-2">
               <Link href={"/blogs/" + blog._id}>
                 <a className="hover:text-brand">{blog.title}</a>
