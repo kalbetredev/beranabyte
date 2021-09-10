7550f27eb0011a321d298de751ddb4febca81ab6
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-09-10 13:17:26 +0300

    tailwind typography custom config

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,10 +23,63 @@ module.exports = {
       dropShadow: {
         error: "0px 0px 15px #FF0000",
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            pre: {
+              backgroundColor: theme("colors.gray.700"),
+            },
+            a: {
+              color: "teal",
+              "&:hover": {
+                color: "#459F9F",
+              },
+            },
+          },
+        },
+        dark: {
+          css: {
+            color: theme("colors.gray.300"),
+            a: {
+              color: "#459F9F",
+              "&:hover": {
+                color: "teal",
+              },
+            },
+            h1: {
+              color: theme("colors.gray.300"),
+            },
+            h2: {
+              color: theme("colors.gray.300"),
+            },
+            h3: {
+              color: theme("colors.gray.300"),
+            },
+            h4: {
+              color: theme("colors.gray.300"),
+            },
+            h5: {
+              color: theme("colors.gray.300"),
+            },
+            h6: {
+              color: theme("colors.gray.300"),
+            },
+            strong: {
+              color: theme("colors.gray.300"),
+            },
+            code: {
+              color: theme("colors.gray.300"),
+            },
+            figcaption: {
+              color: theme("colors.gray.500"),
+            },
+          },
+        },
+      }),
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
