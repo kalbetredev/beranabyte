90b6a52d459a4a1336ca1fb01f3c34110d8384bb
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-09-13 01:32:00 +0300

    old project purge

--- old_src/shared/lib/models/Comment.ts
+++
@@ -1,9 +0,0 @@
-interface Comment {
-  _id: string;
-  blogId: string;
-  authorId: string;
-  text: string;
-  date: string;
-}
-
-export default Comment;
--- old_src/shared/lib/models/SingleLevelMenuItem.ts
+++
@@ -1,9 +0,0 @@
-import MenuItem from "./MenuItem";
-
-class SingleLevelMenuItem extends MenuItem {
-  constructor(label: string, href: string) {
-    super(label, href);
-  }
-}
-
-export default SingleLevelMenuItem;
