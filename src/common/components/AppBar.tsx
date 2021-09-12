70c74fcbf0d8b2341e8c586f1b43997f0a792302
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-09-13 01:24:21 +0300

    top offset moved to app bar component

--- src/common/components/AppBar.tsx
+++ src/common/components/AppBar.tsx
@@ -23,36 +23,39 @@ const AppBar: React.FC = () => {
   };
 
   return (
-    <header className="fixed w-full z-50 top-0 border-b-[1px] shadow-sm flex justify-center items-center bg-white dark:bg-dark dark:border-gray-700">
-      <nav className="h-12 flex justify-between items-center flex-grow max-w-[960px] px-3 sm:px-6">
-        <div className="flex justify-start items-center">
-          <Logo />
-        </div>
-        <div className="flex justify-center items-center">
-          <div className="hidden sm:visible sm:flex border-r-2 pr-2 mr-2">
-            <AppBarPageLinks />
+    <>
+      <header className="fixed w-full z-50 top-0 border-b-[1px] shadow-sm flex justify-center items-center bg-white dark:bg-dark dark:border-gray-700">
+        <nav className="h-12 flex justify-between items-center flex-grow max-w-[960px] px-3 sm:px-6">
+          <div className="flex justify-start items-center">
+            <Logo />
           </div>
-          {auth.user && <UserCircleIcon className="h-6 w-6" />}
-          <IconButton onClick={switchTheme}>
-            {mounted &&
-              (theme === Theme.LIGHT ? (
-                <MoonIcon className="h-6 w-6" />
-              ) : (
-                <SunIcon className="h-6 w-6" />
-              ))}
-          </IconButton>
-          <Menu className="hidden sm:block">
-            <AppBarMenuItems />
-          </Menu>
-          <Menu className="sm:hidden">
-            <>
-              <NavMenuItems />
+          <div className="flex justify-center items-center">
+            <div className="hidden sm:visible sm:flex border-r-2 pr-2 mr-2">
+              <AppBarPageLinks />
+            </div>
+            {auth.user && <UserCircleIcon className="h-6 w-6" />}
+            <IconButton onClick={switchTheme}>
+              {mounted &&
+                (theme === Theme.LIGHT ? (
+                  <MoonIcon className="h-6 w-6" />
+                ) : (
+                  <SunIcon className="h-6 w-6" />
+                ))}
+            </IconButton>
+            <Menu className="hidden sm:block">
               <AppBarMenuItems />
-            </>
-          </Menu>
-        </div>
-      </nav>
-    </header>
+            </Menu>
+            <Menu className="sm:hidden">
+              <>
+                <NavMenuItems />
+                <AppBarMenuItems />
+              </>
+            </Menu>
+          </div>
+        </nav>
+      </header>
+      <div id="top-anchor" className="h-12"></div>
+    </>
   );
 };
 
