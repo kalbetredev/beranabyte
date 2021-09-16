14d700bd14d12594db13f5bf5b8033ba8ee187d3
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-09-16 09:50:15 +0300

    snackbar component

---
+++ src/common/components/Snackbar.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from "react";
+import { CSSTransition } from "react-transition-group";
+
+interface SnackbarProps {
+  isOpen: boolean;
+  onClose: () => void;
+  autoHideDuration?: number;
+  children?: React.ReactNode;
+}
+
+const Snackbar: React.FC<SnackbarProps> = (props: SnackbarProps) => {
+  let autoHideTimer;
+  const { isOpen, onClose, autoHideDuration, children } = props;
+
+  const handleEnter = () => {
+    if (autoHideDuration) setAutoHideTimer();
+  };
+
+  const handleClose = () => {
+    if (onClose) onClose();
+  };
+
+  const setAutoHideTimer = () => {
+    window.clearTimeout(autoHideTimer);
+    autoHideTimer = window.setTimeout(handleClose, autoHideDuration);
+  };
+
+  useEffect(() => {
+    return () => {
+      window.clearTimeout(autoHideTimer);
+    };
+  }, []);
+
+  return (
+    <div className="fixed bottom-12 w-full p-6 z-50">
+      <CSSTransition
+        in={isOpen}
+        timeout={500}
+        appear
+        unmountOnExit
+        classNames="zoom"
+        onEnter={handleEnter}
+        onExit={handleClose}
+      >
+        {children}
+      </CSSTransition>
+    </div>
+  );
+};
+
+export default Snackbar;
