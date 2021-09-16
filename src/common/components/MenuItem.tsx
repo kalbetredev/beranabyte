2e602070931ab1c267d47bd6a44c88da303f26ff
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-09-16 09:49:04 +0300

    menu item component updated to support buttons

--- src/common/components/MenuItem.tsx
+++ src/common/components/MenuItem.tsx
@@ -2,13 +2,29 @@ import React, { ReactNode } from "react";
 import { Menu } from "@headlessui/react";
 import Link from "next/link";
 
-interface MenuItemProps {
+interface LinkMenuItemProps {
   icon: ReactNode;
   label: string;
-  slug: string;
+  slug?: string;
+  renderAsButton?: boolean;
+  onClick?: () => void;
 }
 
-const MenuItem: React.FC<MenuItemProps> = (props: MenuItemProps) => {
+const MenuItem: React.FC<LinkMenuItemProps> = (props: LinkMenuItemProps) => {
+  const Item = ({ children }) => {
+    const classes =
+      "w-full h-full px-2 py-2 flex items-center text-sm cursor-default";
+    return props.renderAsButton ? (
+      <button onClick={props.onClick} className={classes}>
+        {children}
+      </button>
+    ) : (
+      <Link href={props.slug}>
+        <a className={classes}>{children}</a>
+      </Link>
+    );
+  };
+
   return (
     <Menu.Item>
       {({ active }) => (
@@ -17,20 +33,24 @@ const MenuItem: React.FC<MenuItemProps> = (props: MenuItemProps) => {
             active
               ? "bg-brand bg-opacity-50 text-white"
               : "text-gray-900 dark:text-white"
-          } group rounded-md px-2 py-2`}
+          } group rounded-md`}
         >
-          <Link href={props.slug}>
-            <a className="flex items-center w-full text-sm cursor-default">
-              <span className="w-5 h-5 mr-2 flex justify-center items-center">
-                {props.icon}
-              </span>
-              {props.label}
-            </a>
-          </Link>
+          <Item>
+            <span className="w-5 h-5 mr-2 flex justify-center items-center">
+              {props.icon}
+            </span>
+            {props.label}
+          </Item>
         </div>
       )}
     </Menu.Item>
   );
+};
+
+MenuItem.defaultProps = {
+  slug: "",
+  renderAsButton: false,
+  onClick: null,
 };
 
 export default MenuItem;
