6d3a44b94b9b3ccff4af657db9fbbc41bd394ccb
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-10-05 16:03:58 +0300

    add UserAvatar component

---
+++ src/common/components/UserAvatar.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import useSWR from "swr";
+import { USERS_API_ENDPOINT } from "../../api/endpoints";
+import SpinnerIcon from "../../icons/SpinnerIcon";
+import IconButtonError from "./IconButtonError";
+
+type AvatarSize = "small" | "default";
+
+interface UserAvatarProps {
+  userId: string;
+  size?: AvatarSize;
+}
+
+const UserAvatar: React.FC<UserAvatarProps> = (props: UserAvatarProps) => {
+  const { data, error } = useSWR(USERS_API_ENDPOINT + "/" + props.userId);
+  const isLoading = !error && !data;
+
+  if (error) return <IconButtonError />;
+
+  const sizeClasses =
+    props.size == "small" ? "w-7 h-7 text-xs" : "w-9 h-9 text-sm";
+
+  return (
+    <div
+      className={
+        sizeClasses +
+        " mt-1 mr-1 rounded-full border bg-gray-100 dark:bg-gray-600 cursor-default border-gray-300 dark:border-gray-500 flex justify-center items-center text-gray-600 dark:text-gray-300"
+      }
+    >
+      {isLoading ? (
+        <SpinnerIcon />
+      ) : (
+        data.user.email.substring(0, 2).toUpperCase()
+      )}
+    </div>
+  );
+};
+
+UserAvatar.defaultProps = {
+  size: "default",
+};
+
+export default UserAvatar;
