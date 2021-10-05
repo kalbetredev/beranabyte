904bfdd5e2e1089699923e75664da6526e0e8705
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-10-05 11:52:55 +0300

    comments, auth and user api endpoints

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -5,3 +5,16 @@ export const PROJECTS_API_ENDPOINT = `${API_ENDPOINT}/projects`;
 export const BIO_API_ENDPOINT = `${API_ENDPOINT}/bio`;
 export const BLOG_VIEW_COUNT = (blogId: string) =>
   `${BLOGS_API_ENDPOINT}/${blogId}/view-count`;
+export const COMMENTS_API_ENDPOINT = (blogId: string) =>
+  `${BLOGS_API_ENDPOINT}/${blogId}/comments`;
+export const REPLIES_API_ENDPOINT = (commentId: String) =>
+  `${API_ENDPOINT}/comments/${commentId}/replies`;
+
+// Auth
+const AUTH_ENDPOINT = `${API_ENDPOINT}/auth`;
+export const AUTH_REGISTER_API_ENDPOINT = `${AUTH_ENDPOINT}/register`;
+export const AUTH_LOGIN_API_ENDPOINT = `${AUTH_ENDPOINT}/login`;
+
+// Users
+export const USERS_API_ENDPOINT = `${API_ENDPOINT}/users`;
+export const CURRENT_USER_API_ENDPOINT = `${USERS_API_ENDPOINT}/account`;
