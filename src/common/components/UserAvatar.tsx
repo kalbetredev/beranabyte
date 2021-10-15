467709f8b5ebbe1920bf13e2929e36ff4b416c07
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-10-15 11:27:52 +0300

    update user avatar to manage its own state with auth or user Id

--- src/common/components/UserAvatar.tsx
+++ src/common/components/UserAvatar.tsx
@@ -1,40 +1,56 @@
+import { UserIcon } from "@heroicons/react/outline";
 import React from "react";
 import SpinnerIcon from "../../icons/SpinnerIcon";
+import useAuth, { AuthProvider } from "../../modules/auth/hooks/useAuth";
 import useUser from "../hooks/useUser";
 import IconButtonError from "./IconButtonError";
 
 type AvatarSize = "small" | "default";
 
 interface UserAvatarProps {
-  userId: string;
+  userId?: string;
   size?: AvatarSize;
-  hideOnError?: boolean;
 }
 
 const UserAvatar: React.FC<UserAvatarProps> = (props: UserAvatarProps) => {
-  if (props.userId == "") return null;
-  const { user, isLoading, error } = useUser(props.userId);
-
-  if (error) return props.hideOnError ? null : <IconButtonError />;
-
   const sizeClasses =
     props.size == "small" ? "w-7 h-7 text-xs" : "w-9 h-9 text-sm";
+
+  const loadUser = (id: string) => {
+    const { user, isLoading, error } = useUser(id);
+
+    if (isLoading) return <SpinnerIcon />;
+    if (error) return <IconButtonError />;
+    return user.email.substring(0, 2).toUpperCase();
+  };
+
+  let avatar;
+
+  if (props.userId) {
+    avatar = loadUser(props.userId);
+  } else {
+    const auth: AuthProvider = useAuth();
+    avatar = auth.user ? (
+      loadUser(auth.user._id)
+    ) : (
+      <UserIcon className="h-5 w-5" />
+    );
+  }
 
   return (
     <div
       className={
         sizeClasses +
-        " mt-1 mr-1 rounded-full border bg-gray-100 dark:bg-gray-600 cursor-default border-gray-300 dark:border-gray-500 flex justify-center items-center text-gray-600 dark:text-gray-300"
+        " m-1 rounded-full border bg-gray-100 dark:bg-gray-600 cursor-default border-gray-300 dark:border-gray-500 flex justify-center items-center text-gray-600 dark:text-gray-300"
       }
     >
-      {isLoading ? <SpinnerIcon /> : user.email.substring(0, 2).toUpperCase()}
+      {avatar}
     </div>
   );
 };
 
 UserAvatar.defaultProps = {
   size: "default",
-  hideOnError: true,
 };
 
 export default UserAvatar;
