df75ebb03731e5d657b9d49c89feebbd2ae0bfcc
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-10-16 13:14:45 +0300

    add reply components to comment item

--- src/common/components/CommentItem.tsx
+++ src/common/components/CommentItem.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import useUser from "../hooks/useUser";
 import Comment from "../types/Comment";
 import UserAvatar from "./UserAvatar";
 import { format } from "date-fns";
-import { ExclamationIcon } from "@heroicons/react/outline";
+import { ExclamationIcon, ReplyIcon } from "@heroicons/react/outline";
 import CommentItemLoading from "./CommentItemLoading";
+import Reply from "./Reply";
+import ReplyList from "./ReplyList";
 
 interface CommentItemProps {
   comment: Comment;
@@ -16,6 +18,7 @@ const CommentItem: React.FC<CommentItemProps> = (props: CommentItemProps) => {
   const date = new Date(comment.date);
   const formattedDate = format(new Date(comment.date), "MMM d, yyyy");
   const dot = <span className="h-1 w-1 rounded-full bg-gray-400 mx-2"></span>;
+  const [showReply, setShowReply] = useState(false);
 
   if (error)
     return (
@@ -26,6 +29,14 @@ const CommentItem: React.FC<CommentItemProps> = (props: CommentItemProps) => {
     );
 
   if (isLoading) return <CommentItemLoading />;
+
+  const openReply = () => {
+    setShowReply(true);
+  };
+
+  const closeReply = () => {
+    setShowReply(false);
+  };
 
   return (
     <div className="flex mb-3 pb-3">
@@ -46,6 +57,22 @@ const CommentItem: React.FC<CommentItemProps> = (props: CommentItemProps) => {
             __html: comment.content,
           }}
         ></article>
+        <div className="flex-1 mt-1">
+          {showReply ? (
+            <Reply commentId={comment._id} onCancel={closeReply} />
+          ) : (
+            <button
+              onClick={openReply}
+              className="flex justify-center items-center px-2 rounded-md uppercase p-1 leading-tight text-sm text-gray-400 dark:text-gray-500 dark:hover:bg-gray-700 hover:bg-gray-300 hover:bg-opacity-30 dark:hover:bg-opacity-30"
+            >
+              <ReplyIcon className="w-3 h-3 mr-2" />
+              reply
+            </button>
+          )}
+        </div>
+        <div className="mt-3">
+          <ReplyList commentId={comment._id} />
+        </div>
       </div>
     </div>
   );
