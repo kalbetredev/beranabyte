e78b74af715286e11b74d75da9560bf03e43fecf
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-10-18 14:07:40 +0300

    add SignInDialog component

---
+++ src/common/components/SignInDialog.tsx
@@ -0,0 +1,27 @@
+import { XIcon } from "@heroicons/react/outline";
+import React from "react";
+import SignInForm from "./SignInForm";
+
+interface SignInDialogProps {
+  onClose: () => void;
+}
+
+const SignInDialog: React.FC<SignInDialogProps> = (
+  props: SignInDialogProps
+) => {
+  const { onClose } = props;
+
+  return (
+    <div className="relative">
+      <button
+        onClick={props.onClose}
+        className="absolute right-4 top-4 p-1 rounded-full hover:bg-gray-200 active:bg-gray-300 dark:hover:bg-gray-600 dark:active:bg-gray-700"
+      >
+        <XIcon className="w-5 h-5 text-red-400" />
+      </button>
+      <SignInForm onSuccess={onClose} />
+    </div>
+  );
+};
+
+export default SignInDialog;
