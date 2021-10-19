824af45a4332b2b6a8090276a82ba110870b55a3
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-10-19 09:27:18 +0300

    page slug updated to use signup instead of register

--- src/common/constants/page-slugs.ts
+++ src/common/constants/page-slugs.ts
@@ -4,8 +4,8 @@ const pageSlugs = {
   aboutPage: "/about",
   contactPage: "/contact",
   accountSettingsPage: "/account-settings",
-  signUpPage: (continue_to?: string) =>
-    `/register${continue_to != "/" ? "?continue_to=" + continue_to : ""}`,
+  signUpPageSlug: (continue_to?: string) =>
+    `/signup${continue_to != "/" ? "?continue_to=" + continue_to : ""}`,
   signInPageSlug: (continue_to?: string) =>
     `/signin${continue_to != "/" ? "?continue_to=" + continue_to : ""}`,
   passwordRestPageSlug: (continue_to?: string) =>
