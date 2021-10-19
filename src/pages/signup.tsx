a0543a571ce5b123dfbd4dcf6ac78fc6cdeac054
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-10-19 09:26:22 +0300

    add signup page using the new signup form component

---
+++ src/pages/signup.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Page from "../common/layouts/Page";
+import { useRouter } from "next/router";
+import useAuth, { AuthProvider } from "../modules/auth/hooks/useAuth";
+import SignUpForm from "../common/components/SignUpForm";
+
+const SignUpPage = () => {
+  const router = useRouter();
+  const { continue_to } = router.query;
+  const continuePath = continue_to?.toString() || "";
+  const auth: AuthProvider = useAuth();
+  if (auth.user) router.replace("/" + continuePath);
+
+  const onSuccess = () => {
+    router.replace("/" + continuePath);
+  };
+
+  return (
+    <Page>
+      <div className="mt-20 mb-40 mx-auto w-full max-w-sm">
+        <div className="bg-white dark:bg-gray-700 dark:bg-opacity-80 rounded-md separator border shadow overflow-hidden">
+          <SignUpForm onSuccess={onSuccess} continuePath={continuePath} />
+        </div>
+      </div>
+    </Page>
+  );
+};
+
+export default SignUpPage;
