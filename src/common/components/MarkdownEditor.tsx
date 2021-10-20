6db18115ad0ed39a0954f124712f8c39c448fb70
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-10-20 14:19:42 +0300

    make markdown editor a standalone component without form

--- src/common/components/MarkdownEditor.tsx
+++ src/common/components/MarkdownEditor.tsx
@@ -1,27 +1,17 @@
 import React, { useEffect, useRef, useState } from "react";
-import { useForm } from "react-hook-form";
-import SpinnerIcon from "../../icons/SpinnerIcon";
-import FormErrorMessage from "./FormErrorMessage";
 import MarkdownIt from "markdown-it";
 import hljs from "highlight.js";
-import { joiResolver } from "@hookform/resolvers/joi";
-import Joi from "joi";
+import { useFormContext } from "react-hook-form";
+import FormErrorMessage from "./FormErrorMessage";
 
-interface MarkdownEditorProps {
-  placeHolder?: string;
-  submitButtonLabel: string;
-  isLoading?: boolean;
-  callback: (markdown: string, clearContent?: () => void) => void;
-  markdownSchema?: Joi.ObjectSchema<any>;
+interface MarkdownFormEditorProps {
+  placeholder?: string;
+  minLength?: number;
+  maxLength?: number;
+  disabled?: boolean;
+  fromName?: string;
+  id?: string;
 }
-
-type MarkdownForm = {
-  markdown: string;
-};
-
-const defaultMarkdownSchema = Joi.object({
-  markdown: Joi.string().min(10).required(),
-});
 
 const md = new MarkdownIt({
   highlight: function (str, lang) {
@@ -35,40 +25,19 @@ const md = new MarkdownIt({
   },
 });
 
-const MarkdownEditor: React.FC<MarkdownEditorProps> = (
-  props: MarkdownEditorProps
+const MarkdownFormEditor: React.FC<MarkdownFormEditorProps> = (
+  props: MarkdownFormEditorProps
 ) => {
-  const {
-    placeHolder,
-    submitButtonLabel,
-    isLoading,
-    callback,
-    markdownSchema,
-  } = props;
+  const { placeholder, minLength, maxLength, disabled, fromName, id } = props;
+
   const [preview, setPreview] = useState("");
   const [showPreview, setShowPreview] = useState(false);
   const textareaEl = useRef<HTMLTextAreaElement>(null);
-
   const {
     register,
-    handleSubmit,
     formState: { errors },
-    getValues,
-  } = useForm<MarkdownForm>({
-    resolver: joiResolver(
-      markdownSchema ? markdownSchema : defaultMarkdownSchema
-    ),
-  });
-
-  const handlePreview = () => {
-    setShowPreview(true);
-    const markdown = getValues("markdown");
-    setPreview(md.render(markdown));
-  };
-
-  const handleWrite = () => {
-    setShowPreview(false);
-  };
+  } = useFormContext();
+  const { ref, ...rest } = register("markdown");
 
   const handleOnInput = () => {
     if (textareaEl.current) {
@@ -77,18 +46,15 @@ const MarkdownEditor: React.FC<MarkdownEditorProps> = (
     }
   };
 
-  const onSubmit = ({ markdown }) => {
-    callback(markdown, () => {
-      if (textareaEl && textareaEl.current) {
-        textareaEl.current.value = "";
-      }
-    });
+  const handlePreview = () => {
+    setShowPreview(true);
+    const markdown = textareaEl.current.value;
+    setPreview(md.render(markdown));
   };
 
-  const activeBtn =
-    "border-b-0 border-l border-r border-t border-gray-200 dark:border-gray-600 bg-white dark:bg-gray-800";
-
-  const { ref, ...rest } = register("markdown");
+  const handleWrite = () => {
+    setShowPreview(false);
+  };
 
   useEffect(() => {
     if (textareaEl && textareaEl.current) {
@@ -100,99 +66,84 @@ const MarkdownEditor: React.FC<MarkdownEditorProps> = (
     };
   }, [textareaEl]);
 
+  const activeBtn =
+    "border-b-0 border-l border-r border-t border-gray-200 dark:border-gray-600 bg-white dark:bg-gray-800";
+
   return (
     <div className="flex flex-col border border-gray-200 dark:border-gray-600 rounded-md ml-2">
       <div className="flex pt-2 w-full justify-end bg-gray-50 dark:bg-gray-700">
         <div className="flex flex-1 border-b dark:border-gray-600"></div>
-        <button
+        <input
           className={
             "text-xs uppercase rounded-t-lg h-7 w-20 text-gray-500 dark:text-gray-300 hover:text-brand dark:hover:text-brand " +
             (showPreview ? "border-b dark:border-gray-600" : activeBtn)
           }
           onClick={handleWrite}
-          disabled={isLoading}
-        >
-          write
-        </button>
-        <button
+          disabled={disabled}
+          type="button"
+          value="write"
+        />
+        <input
           className={
             "text-xs uppercase rounded-t-lg h-7 w-20 text-gray-500 dark:text-gray-300 hover:text-brand dark:hover:text-brand " +
             (showPreview ? activeBtn : "border-b dark:border-gray-600")
           }
           onClick={handlePreview}
-          disabled={isLoading}
-        >
-          preview
-        </button>
+          disabled={disabled}
+          type="button"
+          value="preview"
+        />
         <div className="flex border-b w-6 dark:border-gray-600"></div>
       </div>
       <div className="flex-1 p-2 pt-3">
-        <form onSubmit={handleSubmit(onSubmit)} name="commentForm">
-          <div className={showPreview ? "block" : "hidden"}>
-            <article
-              className="prose prose-sm break-words dark:prose-dark p-2"
-              dangerouslySetInnerHTML={{ __html: preview }}
-            ></article>
-          </div>
-          <div className="rounded-b-lg rounded-r-lg">
-            <div className={showPreview ? "hidden" : "block"}>
-              <div className="border dark:border-gray-600 rounded-lg focus-within:border-brand-light dark:focus-within:border-brand-dark">
-                <textarea
-                  form="commentForm"
-                  id="comment"
-                  placeholder={placeHolder}
-                  {...rest}
-                  className={
-                    "w-full min-h-[80px] max-h-60 block text-sm rounded-t-lg border-0 border-b dark:border-gray-600 focus:border-gray-300 border-dashed border-gray-300 ring-0 focus:ring-0 overflow-hidden " +
-                    (isLoading
-                      ? "bg-gray-100 dark:bg-gray-700 "
-                      : "bg-white dark:bg-gray-800")
-                  }
-                  maxLength={1000}
-                  ref={(e) => {
-                    ref(e);
-                    textareaEl.current = e;
-                  }}
-                  disabled={isLoading}
-                ></textarea>
-                <div className="text-xs text-gray-400 dark:text-gray-300 px-2 pt-1 pb-1 text-right rounded-b-lg bg-gray-100 dark:bg-gray-700 bg-opacity-40">
-                  Styling with markdown is supported
-                </div>
-              </div>
-            </div>
-            {errors.markdown ? (
-              <div className="mt-3">
-                <FormErrorMessage
-                  message={errors.markdown.message.replace(/['"]+/g, "")}
-                />
-              </div>
-            ) : null}
-            <div className="flex flex-col items-end pt-2">
-              <button
-                type="submit"
+        <div className={showPreview ? "block" : "hidden"}>
+          <article
+            className="prose prose-sm break-words dark:prose-dark p-2"
+            dangerouslySetInnerHTML={{ __html: preview }}
+          ></article>
+        </div>
+        <div className="rounded-b-lg rounded-r-lg">
+          <div className={showPreview ? "hidden" : "block"}>
+            <div className="border dark:border-gray-600 rounded-lg focus-within:border-brand-light dark:focus-within:border-brand-dark">
+              <textarea
+                form={fromName}
+                id={id}
+                placeholder={placeholder}
+                {...rest}
                 className={
-                  "w-28 h-7 text-xs mb-1 flex justify-center items-center py-0 my-0 " +
-                  (isLoading ? "disabled-btn" : "primary-btn")
+                  "w-full min-h-[80px] max-h-60 block text-sm rounded-t-lg border-0 border-b dark:border-gray-600 focus:border-gray-300 border-dashed border-gray-300 ring-0 focus:ring-0 overflow-hidden " +
+                  (disabled
+                    ? "bg-gray-100 dark:bg-gray-700 "
+                    : "bg-white dark:bg-gray-800")
                 }
-                disabled={isLoading}
-              >
-                {isLoading ? (
-                  <div className="m-1 w-4 h-4">
-                    <SpinnerIcon />
-                  </div>
-                ) : null}
-                {submitButtonLabel}
-              </button>
+                maxLength={maxLength}
+                minLength={minLength}
+                ref={(e) => {
+                  ref(e);
+                  textareaEl.current = e;
+                }}
+                disabled={disabled}
+              ></textarea>
+              <div className="text-xs text-gray-400 dark:text-gray-300 px-2 pt-1 pb-1 text-right rounded-b-lg bg-gray-100 dark:bg-gray-700 bg-opacity-40">
+                Styling with markdown is supported
+              </div>
             </div>
           </div>
-        </form>
+          {errors.markdown ? (
+            <div className="mt-3">
+              <FormErrorMessage
+                message={errors.markdown.message.replace(/['"]+/g, "")}
+              />
+            </div>
+          ) : null}
+        </div>
       </div>
     </div>
   );
 };
 
-MarkdownEditor.defaultProps = {
-  isLoading: false,
+MarkdownFormEditor.defaultProps = {
+  disabled: false,
 };
 
-export default MarkdownEditor;
+export default MarkdownFormEditor;
