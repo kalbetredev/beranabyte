cd80d85735034cbda9e37f4eb14a81d9ba0ec8c6
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-10-26 14:10:43 +0300

    refactor subscription form component to use updated form input component

--- src/common/components/SubscriptionForm.tsx
+++ src/common/components/SubscriptionForm.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Joi from "joi";
 import FormInputWithButton from "./FormInputWithButton";
+import { joiResolver } from "@hookform/resolvers/joi";
+import { FormProvider, useForm } from "react-hook-form";
+import useSubscription from "../hooks/useSubscription";
+import useAlert, { AlertProvider } from "../hooks/useAlert";
+
+type SubscriptionForm = {
+  email: string;
+};
 
 const subscriptionFormSchema = Joi.object({
   email: Joi.string()
@@ -9,26 +17,49 @@ const subscriptionFormSchema = Joi.object({
 });
 
 const SubscriptionForm = () => {
-  const onSubmit = (data) => {
-    console.log(data.email);
+  const [isSending, setIsSending] = useState(false);
+  const alert: AlertProvider = useAlert();
+  const { subscribe } = useSubscription();
+
+  const methods = useForm<SubscriptionForm>({
+    resolver: joiResolver(subscriptionFormSchema),
+    defaultValues: { email: "" },
+  });
+
+  const onSubmit = ({ email }) => {
+    setIsSending(true);
+    subscribe((success: boolean) => {
+      setIsSending(false);
+      if (success) {
+        methods.reset();
+        alert.success("Congrats! Your have been successfully subscribed!");
+      } else {
+        alert.error("Error trying to subscribe you. Please try again.");
+      }
+
+      if (success) methods.reset();
+    }, email);
   };
 
   return (
-    <div className="shadow max-w-2xl p-5 rounded-lg border separator bg-brand-light bg-opacity-5 dark:bg-brand-dark dark:bg-opacity-20">
-      <h2 className="text-xl">Subscribe</h2>
-      <p className="text-sm text-gray-400">
-        Subscribe to receive notifications when new articles are published
-      </p>
-      <FormInputWithButton
-        inputType="email"
-        inputName="email"
-        inputPlaceholder="Please Enter Your Email"
-        btnLabel="subscribe"
-        className="mt-3"
-        onSubmit={onSubmit}
-        validationSchema={subscriptionFormSchema}
-      />
-    </div>
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)}>
+        <div className="shadow max-w-2xl p-5 rounded-lg border separator bg-brand-light bg-opacity-5 dark:bg-brand-dark dark:bg-opacity-20">
+          <h2 className="text-xl">Subscribe</h2>
+          <p className="text-sm text-gray-400">
+            Subscribe to receive notifications when new articles are published
+          </p>
+          <FormInputWithButton
+            inputType="email"
+            inputName="email"
+            inputPlaceholder="Please Enter Your Email"
+            btnLabel="subscribe"
+            className="mt-3"
+            isLoading={isSending}
+          />
+        </div>
+      </form>
+    </FormProvider>
   );
 };
 
