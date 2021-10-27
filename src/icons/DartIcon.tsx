aa1a051e8da3b9fa190b66276f4bf3690c46d537
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-10-27 12:07:03 +0300

    change height and width attributes to use percentage

--- src/icons/DartIcon.tsx
+++ src/icons/DartIcon.tsx
@@ -10,8 +10,8 @@ const DartIcon = () => {
         imageRendering="optimizeQuality"
         fillRule="evenodd"
         clipRule="evenodd"
-        height="48px"
-        width="48px"
+        height="100%"
+        width="100%"
         viewBox="0 0 48 48"
       >
         <path
